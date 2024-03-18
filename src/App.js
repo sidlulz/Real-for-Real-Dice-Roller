f2726b7074c6d161a74567d2090d070a5fde00d7
sidlulz <soulejon77@gmail.com>
2024-03-17 20:53:17 -0400

    Imported Dice
    Yep. Fun things

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
+import D6_1 from "./images/D6/D6_1.png";
+import D6_2 from "./images/D6/D6_2.png";
+import D6_3 from "./images/D6/D6_3.png";
+import D6_4 from "./images/D6/D6_4.png";
+import D6_5 from "./images/D6/D6_5.png";
+import D6_6 from "./images/D6/D6_6.png";
+
+
+
 function App() {
   return (
     <div className="App">
